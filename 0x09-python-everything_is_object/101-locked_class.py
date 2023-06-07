0d296ccd878647b5e089be40d624b8bcb7e80ebd
binyusuf <binyusuf30@gmail.com>
2023-06-07 02:37:51 -0700

    chmod

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""
+101-locked_class Module
+"""
+
+
 class LockedClass:
-
-    __slots__ = ['first_name']
-
-    def __init__(self, first_name=''):
-        self.first_name = first_name
+    """ class of locked class """
+    #  define the available slots that can be created
+    __slots__ = ('first_name')
